7f8e59fa50a80e3ec8871bda880c61b3aeb78fb5
Jack200133 <car20593@uvg.edu.gt>
2021-03-09 18:49:36 -0600

    "Primeras separaciones de los inputs y hacer operaciones aritmeticas"

--- src/Evaluar.java
+++ src/Evaluar.java
@@ -1,21 +1,89 @@
 import java.util.HashMap;
 
 public class Evaluar<V,F> {
+    private Operaciones op;
     public Evaluar(){
-
+        op = new Operaciones();
     }
 
-    HashMap<V, F> almacen = new HashMap<V, F>();
+    private HashMap<V, F> almacen = new HashMap<V, F>();
 
     public String Evaluo(String input){
         input = input.replaceAll("\\s","");
         String[] temp = input.split("()");
-        for(int i = temp.length; i > 0;i--){
-            if(temp[i].equals("(")){
+        String res = "";
 
+        if (input.contains("define")) {
+            System.out.print("Funcion");
+
+        } else if (input.contains("quote")) {
+            System.out.print("Quote ");
+
+        } else if (input.contains("setq")) {
+            System.out.print("SetQ");
+
+        } else if (input.contains("cond")) {
+            System.out.print("Condicionañles");
+
+        }else if (input.contains("atom") || input.contains("listp") || input.contains("equal") || input.contains(">") || input.contains("<")) {
+            System.out.print("Predicados");
+
+        } else {
+
+            for (int i = 1; i <= temp.length - 2; i++) {
+                    temp[i-1] = temp[i];
             }
+           String opera = "";
+            for (int i = 0; i <= temp.length-3; i++) {
+                opera+=temp[i];
+            }
+            System.out.println(op.Operar(opera));
+
+
+            for (int i = 0; i <= temp.length - 1; i++) {
+
+                if (temp[i].equals(")")) {
+                    res += temp[i];
+                    break;
+
+                } else {
+                    res += temp[i];
+                }
+            }
+            // (+ 3 (* 2 5) )
+            String[] tamp = res.split("()");
+            String ras = "";
+            for (int i = tamp.length - 1; i >= 0; i--) {
+                if (tamp[i].equals("(")) {
+                    ras += tamp[i];
+                    break;
+                    //Evaluo(res);
+                } else {
+                    ras += tamp[i];
+                }
+            }
+            //System.out.print(res);
+          /**  String rus = ras.replaceAll("\\(", "");
+            String ros = rus.replaceAll("\\)", "");
+
+            String[] timp = ros.split(" ");
+            if (timp[2]== "+"&&timp[2]== "-"){
+
+             }
+            //String opros = op.Operar(timp[0],timp[1],timp[2]);
+            for (int i = 0; i <= timp.length - 1; i++) {
+                System.out.print("\n" + timp[i]);
+            }
+            //System.out.print("\n"+opros);
+            //System.out.print("\n"+answer);
+            // (define vaca (------))
+            //input =  (+ (-(* 1 1) 3) (-5 3))
+           // (* (- 3 2) (+ 1 (/ 1 1))))
+            // res = (+ (-(* 1 1
+            // ras = )* 1 1(
+            return res;
+            */
         }
-        // (+ (-5 3) 6)
         return null;
     }
 
--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,21 +1,66 @@
 public class Operaciones {
 
+    Evaluar ev;
     public Operaciones() {
-
+        ev = new Evaluar();
     }
 
-    public String Operar(String dato1, String dato2 ,char signo){
+    public String Operar(String op){
+        String res = "";
+        String[] temp = op.split("()");
+
+        if(temp[0] == "+" ||temp[0] == "*" ||temp[0] == "-"||temp[0] == "/"){
+            if(isNumber(temp[2]) && isNumber(temp[4]))  {
+                 res = Operar(temp[2], temp[4], temp[0]);
+            }else{
+                String signo = temp[0];
+                for (int i = 1; i <= temp.length - 1; i++) {
+                    temp[i-1] = temp[i];
+                }
+                String eva = "";
+                for (int i = 0; i <= temp.length-3; i++) {
+                    eva+=temp[i];
+                }
+                System.out.print(eva);
+                Operar(ev.Evaluo(eva));
+                //(+ 3 (+ 2 5))
+            }
+        }else
+        {
+            ev.Evaluo(op);
+        }
+        return res;
+    }
+
+    public String Operar(String dato1, String dato2 ,String signo){
         int d1 = Integer.parseInt(dato1);
         int d2 = Integer.parseInt(dato2);
         int res = switch (signo) {
-            case '+' -> (d2 + d1);
-            case '-' -> (d2 - d1);
-            case '*' -> (d2 * d1);
-            case '/' -> (d2 / d1);
+            case "+" -> (d2 + d1);
+            case "-" -> (d2 - d1);
+            case "*" -> (d2 * d1);
+            case "/" -> (d2 / d1);
             default -> 0;
         };
 
         return String.valueOf(res);
     }
 
+    boolean isNumber(String str) {
+        if (str.length() == 0)
+            return false; //To check if string is empty
+
+        if (str.charAt(0) == '-')
+            str = str.replaceFirst("-", "");// for handling -ve numbers
+
+        System.out.println(str);
+
+        str = str.replaceFirst("\\.", ""); //to check if it contains more than one decimal points
+
+        if (str.length() == 0)
+            return false; // to check if it is empty string after removing -ve sign and decimal point
+        System.out.println(str);
+
+        return str.replaceAll("[0-9]", "").length() == 0;
+    }
 }
