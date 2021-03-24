6a1ae40204d533bd22d290f15bbf0c11fca17e95
Jack200133 <car20593@uvg.edu.gt>
2021-03-24 17:37:35 -0600

    "YA FUNCIONA FACTORIAL !!!!"

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Operaciones {
 
 
@@ -8,43 +10,93 @@ public class Operaciones {
 
     public String Operar(String datos){
         String[] temp = datos.split(" ");
-        int res;
+        double res;
         try {
-           res = Integer.parseInt(temp[1]);
-            switch (temp[0]){
-                case  "+":
-                    for (int i = 2; i < temp.length; i++) {
-                        if(!temp[i].equals(" ")){
-                            res += Integer.parseInt(temp[i]);
+            res = Double.parseDouble(temp[1]);
+           if(temp.length > 2) {
+               switch (temp[0]) {
+                   case "+":
+                       for (int i = 2; i < temp.length; i++) {
+                           if (!temp[i].equals(" ")&&!temp[i].equals("*")&&!temp[i].equals("")) {
+                               res += Double.parseDouble(temp[i]);
+                           }
+                       }
+                       break;
+                   case "-":
+                       for (int i = 2; i < temp.length; i++) {
+                           if (!temp[i].equals(" ")&&!temp[i].equals("*")&&!temp[i].equals("")) {
+                               res -= Double.parseDouble(temp[i]);
+                           }
+                       }
+                       break;
+                   case "*":
+                       for (int i = 2; i < temp.length; i++) {
+                           if (!temp[i].equals(" ")&&!temp[i].equals("*")&&!temp[i].equals("")) {
+                               res = res * Double.parseDouble(temp[i]);
+                           }
+                       }
+
+                       break;
+                   case "/":
+                       for (int i = 2; i < temp.length; i++) {
+                           if (!temp[i].equals(" ")&&!temp[i].equals("*")&&!temp[i].equals("")) {
+
+                               res = res / Double.parseDouble(temp[i]);
+                           }
+                       }
+                       break;
+               }
+           }
+           else{
+               return temp[1];
+           }
+        }
+        catch (Exception e){
+            try {
+                res = Double.parseDouble(temp[temp.length-1]);
+                switch (temp[0]) {
+                    case "+":
+                        for (int i = temp.length-2; i > 0; i--) {
+                            if (!temp[i].equals(" ")&&!temp[i].equals("*")&&!temp[i].equals("")) {
+                                res += Double.parseDouble(temp[i]);
+                            }
                         }
-                    }
-                    break;
-                case  "-":
-                    for (int i = 2; i < temp.length; i++) {
-                        if(!temp[i].equals(" ")){
-                            res -= Integer.parseInt(temp[i]);
+                        break;
+                    case "-":
+                        for (int i = temp.length-2; i > 0; i--) {
+                            if (!temp[i].equals(" ")&&!temp[i].equals("*")&&!temp[i].equals("")) {
+                                res -= Double.parseDouble(temp[i]);
+                            }
                         }
-                    }
-                    break;
-                case  "*":
-                    for (int i = 2; i < temp.length; i++) {
-                        if(!temp[i].equals(" ")){
-                            res = res *Integer.parseInt(temp[i]);
+                        break;
+                    case "*":
+                        for (int i = temp.length-2; i > 0; i--) {
+                            if (!temp[i].equals(" ")&&!temp[i].equals("*")&&!temp[i].equals("")) {
+                                res = res * Double.parseDouble(temp[i]);
+                            }else{
+                                res = res * 1.0f;
+                            }
                         }
-                    }
-                    break;
-                case  "/":
-                    for (int i = 2; i < temp.length; i++) {
-                        if(!temp[i].equals(" ")){
-                            res= res / Integer.parseInt(temp[i]);
+
+                        break;
+                    case "/":
+                        for (int i = temp.length-2; i > 0; i--) {
+                            if (!temp[i].equals(" ")&&!temp[i].equals("*")&&!temp[i].equals("")) {
+
+                                res = Double.parseDouble(temp[i]) / res ;
+                            }
                         }
-                    }
-                    break;
+                        break;
+
+                }
+            } catch (Exception f){
+                return datos;
             }
-        } catch (Exception e){
-            return datos;
         }
 
+        if(temp[0].equals("T")||temp[0].equals("t")){
+            return temp[1];
+        }
         return String.valueOf(res);
     }
 
